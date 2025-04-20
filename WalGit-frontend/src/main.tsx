19d6e8732bcf2bf2789dee96fea90bb6ced4fafa
Angleito <arainey555@gmail.com>
2025-04-20 11:58:19 -0700

    feat: Update index.html title and add SPA redirect script; enhance package.json for deployment; implement GitHub Actions for CI/CD; add 404.html for SPA handling

--- WalGit-frontend/src/main.tsx
+++ WalGit-frontend/src/main.tsx
@@ -1,13 +1,13 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="dark bg-background min-h-screen text-foreground">
+    <BrowserRouter basename="/walgit">
       <App />
-    </div>
+    </BrowserRouter>
   </React.StrictMode>,
 )
